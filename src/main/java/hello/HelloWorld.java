908b78c3cb86449e513eb5636fc3a84046cbcacd
Cenk Kalpakoğlu <cenk@kondukto.io>
2021-12-13 16:03:08 +0300

    add code

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,22 @@
+package hello;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class HelloWorld {
+
+  private static final Logger logger = LogManager.getLogger(HelloWorld.class);
+
+  public static void main(String[] args) {
+    Greeter greeter = new Greeter();
+    System.out.println(greeter.sayHello());
+
+    logger.trace("We've just greeted the user!");
+    logger.debug("We've just greeted the user!");
+    logger.info("We've just greeted the user!");
+    logger.warn("We've just greeted the user!");
+    logger.error("We've just greeted the user!");
+    logger.fatal("We've just greeted the user!");
+
+  }
+}
